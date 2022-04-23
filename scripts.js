acde0c5a85e6eddcbde2f539cc50abea5aef914c
Gustavo Menezes <91099666+guhmenezes@user.noreply.github.com>
2022-04-23 17:31:21 -0300

    placar

--- scripts.js
+++ scripts.js
@@ -1,12 +1,35 @@
 const dino = document.querySelector('.dino');
 const bg = document.querySelector('.bg');
+const pontuacao = document.querySelector('.pontuacao');
 let isJumping = false;
 let position = 0;
 
-function handleKeyUp(event){
-  if (event.keyCode === 32)
+function placar(){
+  if (+pontuacao.textContent === 0){
+  var count = setInterval(() => {
+    count += 8; 
+    console.log(count)
+    pontuacao.textContent = '00000'+count
+  },1000)
+  } else if (count > 100){
+    var count = setInterval(() => {
+      count += 16; 
+      console.log(count)
+      pontuacao.textContent = count
+    },1000)
+  }
+
+}
+
+function handleJump(event){
+  console.log(event)
+  if (event.keyCode === 32 || event.type === 'click'){
     if (!isJumping) 
     jump();
+  }
+  else if (event.keyCode === 16 || event.type ==='double click'){
+    superJump();
+  }
 }
 
 function jump(){
@@ -31,10 +54,32 @@ function jump(){
   }, 10)
 }
 
+function superJump(){
+  // isJumping = true;
+  let upInterval = setInterval(() => {
+    if (position >= 300){
+      clearInterval(upInterval);
+
+      let downInterval = setInterval(() => {
+        if (position <= 0){
+          clearInterval(downInterval);
+          isJumping = false;
+        } else {
+          position -= 10;
+          dino.style.bottom = position + 'px';
+        }
+      }, 40)
+    } else {
+      position += 15;
+      dino.style.bottom = position + 'px';
+    }
+  }, 10)
+}
+
 function createCactus(){
   const cactus = document.createElement('div');
-  let cactusPosition = 1000;
-  let randomTime = Math.random() * 3000 + 100;
+  let cactusPosition = 750;
+  let randomTime = Math.random() * 3000 + 800;
 
   cactus.classList.add('cactus');
   cactus.style.left = cactusPosition + 'px';
@@ -58,4 +103,7 @@ function createCactus(){
 }
 
 createCactus();
-document.addEventListener('keyup', handleKeyUp)
+placar();
+// document.addEventListener('keyup', handleJump);
+// document.addEventListener('click', handleJump);
+['click', 'keyup'].forEach(evt => document.addEventListener(evt, handleJump))
